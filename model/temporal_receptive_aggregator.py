3b4ff7b4723115e1c22240f5b9d811b16565030f
qyw7600-sudo <qyw7600@gmail.com>
2025-12-21 19:57:14 +0800

    Replace casual_conv_depth with receptive_depth

--- model/temporal_receptive_aggregator.py
+++ model/temporal_receptive_aggregator.py
@@ -22,11 +22,11 @@ class TemporalReceptiveAaggregator(nn.Module):
         self.device = args.device
         self.in_channels = in_channels
         self.hidden_channels = out_channels
-        self.casual_conv_depth = args.casual_conv_depth
+        self.receptive_depth = args.receptive_depth
         self.casual_conv_kernel_size = args.casual_conv_kernel_size
         self.scales=args.scales
         self.dilated_stack = nn.ModuleList([EfficientMLHA(manifold=self.manifold,k_hidden=self.c, in_channels=self.in_channels, out_channels=self.hidden_channels,casual_kernel_size=self.casual_conv_kernel_size,dilation=self.casual_conv_kernel_size**layer,heads_ratio=1.0, dim=4, scales=self.scales)
-             for layer in range(self.casual_conv_depth)])
+             for layer in range(self.receptive_depth)])
 
     def forward(self, x):
         skips = []
