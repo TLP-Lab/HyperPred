67e1fa5e4fc9c79021ca0038ea2b166b78f5dc68
qyw7600-sudo <qyw7600@gmail.com>
2025-12-21 19:53:07 +0800

    Rename parameter for diffusion steps in function

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -24,38 +24,30 @@ def loader(args):
         logger.info('{} does not exist, exiting...')
         return None
 
-def prepare_diffusion_edge_index(data, spatial_dilated_factors, device, top_k=None, eps=0.005, t: float = 1.0):
+def prepare_diffusion_edge_index(data, diffusion_steps, device, top_k=None, eps=0.005, t: float = 1.0):
     diffusion_edge_index_list = []
     for edge_index in tqdm(data['edge_index_list']):
-        # 原始邻接矩阵
         adj = coo_matrix(([1] * len(edge_index[0]), (list(edge_index[0]), list(edge_index[1]))), dtype=np.float32)
         adj = adj.tocsr() + adj.transpose().tocsr()
         num_nodes = adj.shape[0]
 
-        dilated_edge_index = []
-        for k in spatial_dilated_factors:
-            # 对称归一化邻接矩阵
+        diffusion_edge_index = []
+        for k in diffusion_steps:
             A_tilde = adj + eye(num_nodes, dtype=np.float32, format='csr')
             deg = np.array(A_tilde.sum(1)).flatten()
             deg[deg == 0] = 1.0
             D_inv_sqrt = sp.diags(1.0 / np.sqrt(deg))
             H = D_inv_sqrt.dot(A_tilde).dot(D_inv_sqrt)
-
-            # 拉普拉斯矩阵
             L = eye(num_nodes, format='csr', dtype=np.float32) - H
-
-            # 计算矩阵指数 exp(-tL) (泰勒展开)
             power = eye(num_nodes, format='csr', dtype=np.float32)
             M = coo_matrix(eye(num_nodes, dtype=np.float32))
             coef = 1.0
-            for n in range(1, k + 1):  #
+            for n in range(1, k + 1): 
                 power = power.dot(L)
                 coef = coef * (-t) / n
                 M = M + coef * power
             M = M.tocoo()
             M.eliminate_zeros()
-
-            # Top-k 或 eps 稀疏化
             if top_k is not None:
                 new_rows, new_cols, new_data = [], [], []
                 for i in range(num_nodes):
@@ -77,16 +69,13 @@ def prepare_diffusion_edge_index(data, spatial_dilated_factors, device, top_k=No
                     M = coo_matrix((M.data[mask], (M.row[mask], M.col[mask])), shape=(num_nodes, num_nodes))
                 else:
                     M = coo_matrix((num_nodes, num_nodes))
-
-            # 转成 edge_index
             coords = np.vstack((M.row, M.col))
             if coords.shape[1] > 0:
                 np.random.shuffle(coords.T)
             coords_tensor, _ = remove_self_loops(torch.tensor(coords, dtype=torch.long))
-            dilated_edge_index.append(coords_tensor.to(device))
+            diffusion_edge_index.append(coords_tensor.to(device))
 
-        diffusion_edge_index_list.append(dilated_edge_index)
-
+        diffusion_edge_index_list.append(diffusion_edge_index)
     return diffusion_edge_index_list
 
 
