c01c6478cf0dea77cfcef49f0181629809c0b501
qyw7600-sudo <qyw7600@gmail.com>
2025-12-22 09:03:07 +0800

    Init
    Removed print statements for debugging and cleaned up code.

--- loss.py
+++ loss.py
@@ -49,29 +49,20 @@ class ReconLoss(nn.Module):
         return pos_loss + neg_loss
 
     def predict(self, z, pos_edge_index, neg_edge_index):
-        print('z的大小：',z.shape)
         decoder = self.hyperdecoder if self.use_hyperdecoder else self.decoder
         pos_y = z.new_ones(pos_edge_index.size(1)).to(self.device)
         neg_y = z.new_zeros(neg_edge_index.size(1)).to(self.device)
-        print('pos_y:',pos_y.shape)
-        print('neg_y:',neg_y.shape)
         y = torch.cat([pos_y, neg_y], dim=0)
-        print('真实矩阵大小：',y.shape)
         pos_pred = decoder(z, pos_edge_index)
         neg_pred = decoder(z, neg_edge_index)
         pred = torch.cat([pos_pred, neg_pred], dim=0)
-        print('预测矩阵大小',pred.shape)
         y, pred = y.detach().cpu().numpy(), pred.detach().cpu().numpy()
-        # 计算 ROC AUC 和 AP
         auc = roc_auc_score(y, pred)
         ap = average_precision_score(y, pred)
 
-        #计算 ER（Error Rate）
-        pred_label = (pred >= 0.4).astype(int)  # 二值化
+        pred_label = (pred >= 0.4).astype(int)
         error_count = (pred_label != y).sum()
         er = error_count / len(y)
-        # print(f'错误预测数量: {error_count} / 总数: {len(y_np)}')
-        # print(f'ER (Error Rate): {er:.4f}')
         return auc, ap,er
 
 
