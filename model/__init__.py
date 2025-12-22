38aac5cf7c34d12e0332545e76ca1612b02c4a02
qyw7600-sudo <qyw7600@gmail.com>
2025-12-22 09:46:46 +0800

    Init

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,4 @@
-from .hyperpred import HyperPre
+from .hyperpred import HyperPred
 from .graph_diffusion_encoder import GraphDiffusionEncoder
 # from .temporal_casual_conv import TemporalCasualConv
 from .temporal_receptive_aggregator import TemporalReceptiveAaggregator
